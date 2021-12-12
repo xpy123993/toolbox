e820e320dcd89cd6bde09c3b6e5b5aa9b712890e
xpy123993 <xpy123993@gmail.com>
2021-12-12 15:03:05 -0800

    Add some tracking info.

--- cmd/taskmaster/task-worker/main.go
+++ cmd/taskmaster/task-worker/main.go
@@ -7,17 +7,22 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os/exec"
 	"time"
 
 	"github.com/xpy123993/toolbox/pkg/task"
+	"golang.org/x/net/trace"
 )
 
 var (
 	taskMasterAddress = flag.String("task-master-address", "localhost:8080", "The address to access task master service.")
 	defaultTimeout    = flag.Duration("execution-timeout", time.Hour, "Timeout to execute a task.")
 	idleCycle         = flag.Duration("idle-cycle", time.Minute, "The cycle to pull a task if task master returns no task available.")
+
+	statusAddress = flag.String("status-address", ":0", "The address to show the current status")
+	finishCount   = 0
 )
 
 // ShellTask specifies the structure to execute a shell command.
@@ -42,20 +47,36 @@ func workerRoutinue() error {
 	if err := json.Unmarshal([]byte(curTask.Data), &curShellTask); err != nil {
 		return fmt.Errorf("unknown task format: %s", curTask.Data)
 	}
+	tracker := trace.New("Worker", fmt.Sprintf("working on task `%s`", curTask.ID))
+	defer tracker.Finish()
 	log.Printf("working on task `%s`", curTask.ID)
+	tracker.LazyPrintf("command: %s", curTask.Data)
+	tracker.LazyPrintf("deadline: %v", curTask.AvailableTime)
 	ctx, cancelFn := context.WithDeadline(context.Background(), curTask.AvailableTime.Add(-time.Second))
 	defer cancelFn()
 	cmd := exec.CommandContext(ctx, curShellTask.Base, curShellTask.Arguments...)
-	if _, err := cmd.CombinedOutput(); err != nil {
+	if data, err := cmd.CombinedOutput(); err != nil {
+		tracker.LazyPrintf(err.Error())
+		tracker.SetError()
 		return err
+	} else {
+		truncText := data
+		if len(data) > 64 {
+			truncText = data[:64]
+		}
+		tracker.LazyPrintf("Result: %s", truncText)
 	}
 	if resp, err = http.DefaultClient.Get(fmt.Sprintf("http://%s/finish?task_id=%s", *taskMasterAddress, curTask.ID)); err != nil || resp.StatusCode != 200 {
 		if resp.StatusCode != 200 {
 			data, _ := io.ReadAll(resp.Body)
+			tracker.LazyPrintf(string(data))
+			tracker.SetError()
 			return fmt.Errorf("task master returns an error status (%d): %s", resp.StatusCode, data)
 		}
 		return err
 	}
+	finishCount++
+	tracker.LazyPrintf("commited")
 	log.Printf("task `%s` is commited", curTask.ID)
 	return nil
 }
@@ -69,7 +90,21 @@ func worker() {
 	}
 }
 
+func startStatusService() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(rw, "Finish count: %d", finishCount)
+	})
+	listener, err := net.Listen("tcp", *statusAddress)
+	if err != nil {
+		log.Printf("failed to start status reporting service: %v", err)
+		return
+	}
+	log.Printf("status page is available at %s", listener.Addr().String())
+	http.Serve(listener, nil)
+}
+
 func main() {
 	flag.Parse()
+	go startStatusService()
 	worker()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/xpy123993/toolbox
 
 go 1.17
 
-require github.com/google/uuid v1.3.0
+require (
+	github.com/google/uuid v1.3.0
+	golang.org/x/net v0.0.0-20211209124913-491a49abca63
+)
